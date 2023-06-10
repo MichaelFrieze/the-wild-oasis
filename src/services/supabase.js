65a54b10b07e265d1e8f57c6d3f553f4fe45775a
Michael Frieze <michaelfrieze@icloud.com>
2023-06-10 09:25:04 -0400

    connecting supabase and using it to fetch cabins

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://hgosoqziylahwzcanrvj.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhnb3NvcXppeWxhaHd6Y2FucnZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYzNDQyMjgsImV4cCI6MjAwMTkyMDIyOH0.N0UybgIg6sGRyOmB2io2uhJn3DENfN0WMZjSo5uB--E';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
